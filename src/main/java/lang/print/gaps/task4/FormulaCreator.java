2875e804586c8aadb7a103ab453c1937e974b753
ShulpinP <pavelshulpin6@gmail.com>
2022-06-17 22:01:53 +0300

    primitives

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a=1;
+        byte b=-1;
+        double c = (9*(a*a)-5*b+2+a-7)*((a+b-(4*a*b)/2));
     }
 }
